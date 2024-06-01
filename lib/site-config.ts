27c9afb2b974f6d0e33d1bc1d53849f810d255d2
heygyun <gyun@custom-mate.com>
2024-06-01 21:01:51 +0900

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -16,7 +16,8 @@ export interface SiteConfig {
   newsletter?: string
   youtube?: string
   zhihu?: string
-  mastodon?: string;
+  mastodon?: string
+  instagram?: string // 여기 추가
 
   defaultPageIcon?: string | null
   defaultPageCover?: string | null
